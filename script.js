f3184ceebdd64de211956a1ec6465bff054269a2
Corey Hedberg <chedberg@gmail.com>
2020-04-05 18:53:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ window.onload = () => {
 document.getElementById("play-button").addEventListener("click", () => {
   document.getElementById("game-area").style.visibility = "visible";
 });
+
+let canvas; // Used for handle on the information about the dimensions
+let canvasContext; // Underlying graphical information to draw to
+
+canvas = document.getElementById("game-canvas");
+canvasContext = canvas.getContext("2d");
+canvasContext.fillStyle = "black";
+canvasContext.fillRect(0, 0, canvas.clientWidth, canvas.height);
