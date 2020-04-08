0896f098aba4094330aef5dacaa3b6e05f3130cb
Corey Hedberg <chedberg@gmail.com>
2020-04-07 21:23:35 -0400

    Some good work done today. Get the apple placing randomly.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-// to set the game as hidden.
-// https://www.w3schools.com/jsref/prop_style_visibility.asp
-
-window.onload = () => {
-  document.getElementById("game-area").style.visibility = "hidden";
-};
-
-document.getElementById("play-button").addEventListener("click", () => {
-  document.getElementById("game-area").style.visibility = "visible";
-});
-
 let canvas; // Used for handle on the information about the dimensions
 let canvasContext; // Underlying graphical information to draw to
 
 canvas = document.getElementById("game-canvas");
 canvasContext = canvas.getContext("2d");
 canvasContext.fillStyle = "black";
-canvasContext.fillRect(0, 0, canvas.clientWidth, canvas.height);
+canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+/* canvasContext.fillStyle = "#00205b";
+canvasContext.fillRect(100, 200, 10, 10); */
+
+document.getElementById("place-apple").addEventListener("click", placeApple);
+
+function placeApple() {
+  let randomX = Math.floor(Math.random() * canvas.width + 1);
+  console.log(`randomX : ${randomX}`);
+  let randomY = Math.floor(Math.random() * canvas.height + 1);
+  console.log(`randomY : ${randomY}`);
+  canvasContext.fillStyle = "red";
+  canvasContext.fillRect(randomX, randomY, 10, 10);
+}
