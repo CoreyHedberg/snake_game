3479877c6a44883bccc84e8e66d9c6cd8eb8ad56
Corey Hedberg <chedberg@gmail.com>
2020-04-12 14:28:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let canvas; // Used for handle on the information about the dimensions
-let canvasContext; // Underlying graphical information to draw to
+// let canvas; // Used for handle on the information about the dimensions
+// let canvasContext; // Underlying graphical information to draw to
 
-canvas = document.getElementById("game-canvas");
-canvasContext = canvas.getContext("2d");
+let canvas = document.getElementById("game-canvas");
+let canvasContext = canvas.getContext("2d");
 canvasContext.fillStyle = "black";
 canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 /* canvasContext.fillStyle = "#00205b";
 canvasContext.fillRect(100, 200, 10, 10); */
 
-document.getElementById("place-apple").addEventListener("click", placeApple);
+document
+  .getElementById("place-apple")
+  .addEventListener("click", placeRandomApple);
 
-function placeApple() {
+function placeRandomApple() {
   let randomX = Math.floor(Math.random() * canvas.width + 1);
   console.log(`randomX : ${randomX}`);
   let randomY = Math.floor(Math.random() * canvas.height + 1);
