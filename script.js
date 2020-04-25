3238cc4817d692f19345a5074372969b962e1b1b
Corey Hedberg <chedberg@gmail.com>
2020-04-25 15:30:18 -0400

    Some good work done on the project.

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 let canvas = document.getElementById("game-canvas");
-let canvasContext = canvas.getContext("2d");
-canvasContext.fillStyle = "black";
-canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+let context = canvas.getContext("2d");
+
+let foodX = 0;
+let foodY = 0;
+let snakeX = 300;
+let snakeY = 300;
+let snake = [300, 300];
+const LEFT_KEY = 37;
+const UP_KEY = 38;
+const RIGHT_KEY = 39;
+const DOWN_KEY = 40;
+
+const CANVAS_BACKGROUND_COLOR = "#F5F5DC";
+const SNAKE_COLOR = "#004200";
+const SNAKE_BORDER_COLOR = "#006600";
+const APPLE_COLOR = "#FF0000";
+const APPLE_BORDER_COLOR = "#820000";
+
+let gameStarted = false;
+
+window.onload = () => {
+  drawCanvas();
+  drawSnakeSegment();
+  beforeGameMessage();
+};
 
 window.addEventListener("keydown", (e) => {
   switch (e.keyCode) {
@@ -9,6 +31,8 @@ window.addEventListener("keydown", (e) => {
       // left key pressed
       e.preventDefault();
       console.log(`left`);
+      snakeX -= 10;
+      drawSnakeSegment();
       break;
     case 38:
       // up key pressed
@@ -28,15 +52,44 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
+// Draws the canvas
+function drawCanvas() {
+  context.fillStyle = CANVAS_BACKGROUND_COLOR;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawSnakeSegment() {
+  context.fillStyle = SNAKE_COLOR;
+  context.strokestyle = SNAKE_BORDER_COLOR;
+  context.fillRect(snakeX, snakeY, 10, 10);
+  context.strokeRect(snakeX, snakeY, 10, 10);
+  console.log(snake[0]);
+  console.log(snake[1]);
+}
+
 document
   .getElementById("place-apple")
   .addEventListener("click", placeRandomApple);
 
 function placeRandomApple() {
-  let randomX = Math.floor(Math.random() * canvas.width + 1);
-  console.log(`randomX : ${randomX}`);
-  let randomY = Math.floor(Math.random() * canvas.height + 1);
-  console.log(`randomY : ${randomY}`);
-  canvasContext.fillStyle = "red";
-  canvasContext.fillRect(randomX, randomY, 10, 10);
+  foodX = randomTen(0, canvas.width - 10);
+  console.log(`foodX : ${foodX}`);
+  foodY = randomTen(0, canvas.height - 10);
+  console.log(`foodY : ${foodY}`);
+  context.fillStyle = APPLE_COLOR;
+  context.strokesytle = APPLE_BORDER_COLOR;
+  context.fillRect(foodX, foodY, 10, 10);
+  context.strokeRect(foodX, foodY, 10, 10);
+}
+
+function beforeGameMessage() {
+  drawCanvas();
+  context.fillStyle = "#000000";
+  context.font = "30px PixelBoy";
+  context.fillText(`Press an arrow key to begin.`, 124, 300);
+  gameStarted = true;
+}
+
+function randomTen(min, max) {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
 }
