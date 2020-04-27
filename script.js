f6d3ac6390098e0bba14a796c831f6be2924c5b7
Corey Hedberg <chedberg@gmail.com>
2020-04-26 20:19:11 -0400

    Snake is now moving with the arrow keys.

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 let canvas = document.getElementById("game-canvas");
 let context = canvas.getContext("2d");
 
-let gameSpeed = 100;
 let foodX = 0;
 let foodY = 0;
-
-const LEFT_KEY = 37;
-const UP_KEY = 38;
-const RIGHT_KEY = 39;
-const DOWN_KEY = 40;
 
 let snake = [{ x: 300, y: 300 }];
 console.log(snake[0].x);
 console.log(snake[0].y);
-let directionX = 10;
-let directionY = 10;
+let headX = snake[0].x;
+let headY = snake[0].y;
+let directionX = 0;
+let directionY = 0;
 
 const CANVAS_BACKGROUND_COLOR = "#F5F5DC";
 const SNAKE_COLOR = "#004200";
@@ -22,10 +18,16 @@ const SNAKE_BORDER_COLOR = "#006600";
 const APPLE_COLOR = "#FF0000";
 const APPLE_BORDER_COLOR = "#820000";
 
+let score = 0;
+
 let gameStarted = false;
 
 window.onload = () => {
-  drawEverything();
+  createRandomApple();
+  main();
+  // drawCanvas();
+  // drawSnake();
+  // drawSnakeSegment();
 };
 
 window.addEventListener("keydown", (e) => {
@@ -34,30 +36,37 @@ window.addEventListener("keydown", (e) => {
       // left key pressed
       e.preventDefault();
       console.log(`left`);
-      moveSnake(e.keyCode);
+      changeDirection(e.keyCode);
       break;
     case 38:
       // up key pressed
       e.preventDefault();
       console.log(`up`);
+      changeDirection(e.keyCode);
       break;
     case 39:
       // right key pressed
       e.preventDefault();
       console.log(`right`);
+      changeDirection(e.keyCode);
       break;
     case 40:
       // down key pressed
       e.preventDefault();
       console.log(`down`);
+      changeDirection(e.keyCode);
       break;
   }
 });
 
-function drawEverything() {
-  drawCanvas();
-  drawSnake();
-  setInterval(drawSnakeSegment, 100);
+function main() {
+  setTimeout(function gamePlay() {
+    drawCanvas();
+    drawRandomApple();
+    drawSnake();
+    advanceSnake();
+    main();
+  }, 100);
 }
 
 // Draws the canvas
@@ -70,26 +79,64 @@ function drawSnake() {
   snake.forEach(drawSnakeSegment);
 }
 
-function drawSnakeSegment(snake) {
+function drawSnakeSegment(snakeSegment) {
   context.fillStyle = SNAKE_COLOR;
   context.strokestyle = SNAKE_BORDER_COLOR;
-  context.fillRect(snake[0].x, snake[0].y, 10, 10);
-  context.strokeRect(snake[0].x, snake[0].y, 10, 10);
+  context.fillRect(snakeSegment.x, snakeSegment.y, 10, 10);
+  context.strokeRect(snakeSegment.x, snakeSegment.y, 10, 10);
 }
 
-function moveSnake(movement) {
-  console.log(movement);
-  // move snake
-  if (movement === 37) {
-    snake[0].x -= directionX;
-    console.log(snake[0].x);
+function advanceSnake() {
+  const head = { x: snake[0].x + directionX, y: snake[0].y + directionY };
+  snake.unshift(head);
+
+  if (headX === foodX && headY === foodY) {
+    score++;
+    createRandomApple();
+  } else {
+    snake.pop();
+  }
+}
+
+function changeDirection(e) {
+  const goingUp = directionY === -10;
+  const goingDown = directionY === 10;
+  const goingRight = directionX === 10;
+  const goingLeft = directionX === -10;
+  if (e === 37 && !goingRight) {
+    directionX = -10;
+    directionY = 0;
+  }
+  if (e === 38 && !goingDown) {
+    directionX = 0;
+    directionY = -10;
+  }
+  if (e === 39 && !goingLeft) {
+    directionX = 10;
+    directionY = 0;
+  }
+  if (e === 40 && !goingUp) {
+    directionX = 0;
+    directionY = 10;
   }
 }
 
 // Good function, ready to use.
-function placeRandomApple() {
+function createRandomApple() {
   foodX = Math.floor(Math.random() * 60) * 10;
   foodY = Math.floor(Math.random() * 60) * 10;
+  console.log(`foodX : ${foodX}`);
+  console.log(`foodY : ${foodY}`);
+
+  snake.forEach(function isFoodOnSnake(part) {
+    appleIsOnSnake = part.x === foodX && part.y === foodY;
+    if (appleIsOnSnake) {
+      createRandomApple();
+    }
+  });
+}
+
+function drawRandomApple() {
   context.fillStyle = APPLE_COLOR;
   context.strokesytle = APPLE_BORDER_COLOR;
   context.fillRect(foodX, foodY, 10, 10);
