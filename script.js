ac27752dff1f66c0d0d98d52cac5bec08f924337
Corey Hedberg <chedberg@gmail.com>
2020-04-28 06:30:38 -0400

    Getting close to finishing the game.

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let foodX = 0;
 let foodY = 0;
 
 let snake = [{ x: 300, y: 300 }];
-console.log(snake[0].x);
-console.log(snake[0].y);
 let headX = snake[0].x;
+console.log(`headX : ${headX}`);
 let headY = snake[0].y;
+console.log(`headY : ${headY}`);
 let directionX = 0;
 let directionY = 0;
 
@@ -19,11 +19,14 @@ const APPLE_COLOR = "#FF0000";
 const APPLE_BORDER_COLOR = "#820000";
 
 let score = 0;
+document.getElementById("current-score").innerHTML = score;
 
 let gameStarted = false;
+let gameIsOver = false;
 
 window.onload = () => {
   createRandomApple();
+  isGameOver();
   main();
   // drawCanvas();
   // drawSnake();
@@ -60,11 +63,13 @@ window.addEventListener("keydown", (e) => {
 });
 
 function main() {
-  setTimeout(function gamePlay() {
+  if (gameIsOver === true) return;
+  setTimeout(() => {
     drawCanvas();
     drawRandomApple();
     drawSnake();
-    advanceSnake();
+    growSnake();
+    isGameOver();
     main();
   }, 100);
 }
@@ -86,12 +91,13 @@ function drawSnakeSegment(snakeSegment) {
   context.strokeRect(snakeSegment.x, snakeSegment.y, 10, 10);
 }
 
-function advanceSnake() {
+function growSnake() {
   const head = { x: snake[0].x + directionX, y: snake[0].y + directionY };
   snake.unshift(head);
 
-  if (headX === foodX && headY === foodY) {
+  if (head.x === foodX && head.y === foodY) {
     score++;
+    document.getElementById("current-score").innerHTML = score;
     createRandomApple();
   } else {
     snake.pop();
@@ -141,6 +147,30 @@ function drawRandomApple() {
   context.strokesytle = APPLE_BORDER_COLOR;
   context.fillRect(foodX, foodY, 10, 10);
   context.strokeRect(foodX, foodY, 10, 10);
+}
+
+function isGameOver() {
+  for (var i = 1; i < snake.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      gameOver();
+    }
+    if (
+      snake[0].x < 0 ||
+      snake[0].x > canvas.width - 10 ||
+      snake[0].y < 0 ||
+      snake[0].y > canvas.height - 10
+    ) {
+      gameOver();
+    }
+  }
+}
+
+function gameOver() {
+  // save
+  gameIsOver = true;
+  score = 0;
+  document.getElementById("current-score").innerHTML = score;
+  beforeGameMessage();
 }
 
 // Good function, ready to use.
